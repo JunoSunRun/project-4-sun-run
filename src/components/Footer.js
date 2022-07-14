97255d0327ec7171a4302037ab3103b8ffc8d98d
Rob Petrungaro <rob.petrungaro@gmail.com>
2022-07-14 12:31:36 -0400

    removed unused files, minor adjustments to media queries, added portfolio links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
     return(
         <footer>
             <div className="footerCloud">
-                <p>Created by: "Our names here" at <a href="https://www.junocollege.com/" target="_blank" rel="noreferrer">Juno College</a></p>
+                <p>Created at <a href="https://www.junocollege.com/" target="_blank" rel="noreferrer">Juno College</a> by <a href="https://courtfolio.ca" target="_blank" rel="noreferrer">Court</a>, <a href="https://joegencarelli.com" target="_blank" rel="noreferrer">Joe</a>, <a href="https://www.kianaomoerah.com/" target="_blank" rel="noreferrer">Kiana</a> & <a href="https://robpetrungaro.com" target="_blank" rel="noreferrer">Rob</a> at </p>
                 <p>API by <a href="https://sunrise-sunset.org" target="_blank" rel="noreferrer">Sunrise Sunset</a></p>
             </div>
         </footer>
